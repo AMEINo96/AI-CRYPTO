4d711495cffb1a2fc22bce9ca26af522718f61bf
AMEINo123 <muhammadamein6@gmail.com>
2025-12-03 18:54:17 +0500

    Refactor Firebase config to use env vars

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,10 @@
 
 export const firebaseConfig = {
-  "projectId": "studio-5113748038-88983",
-  "appId": "1:119371487952:web:683bd4f5a3e40c18329cd5",
-  "apiKey": "AIzaSyCmUZjN0suRLb95vnWxUGcqxKye6Gb0frA",
-  "authDomain": "studio-5113748038-88983.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "119371487952"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
